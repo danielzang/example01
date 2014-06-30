e8f2a182a87af784dd9c3d029290ba87c8e3d132
user <user@usertekiMacBook-Pro.local>
2014-06-30 13:59:18 +0800

    Initial Commit

---
+++ example01/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  example01
+//
+//  Created by user on 6/30/14.
+//  Copyright (c) 2014 sword. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
